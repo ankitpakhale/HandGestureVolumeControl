4a3552242e538640a34728a05906efe2ba8c59d4
AnkitPakhale <akp3067@gmail.com>
2023-05-05 18:26:19 +0530

    Added: Volume Bar and volume percentage

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -21,13 +21,16 @@ detector = htm.handDetector(detectionCon=0.7)
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+
 volume = interface.QueryInterface(IAudioEndpointVolume)
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
-
 volRange = volume.GetVolumeRange()
-volume.SetMasterVolumeLevel(-5, None)
 
+# Default Values    
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 0
+volPer = 0
 
 while True:
     success, img = cap.read()
@@ -35,8 +38,6 @@ while True:
 
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
-
         # getting points to create line
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
@@ -44,23 +45,62 @@ while True:
         # getting center of the line
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+        # thumb and first finger circle
+        cv2.circle(img, (x1, y1), 10, (0, 255, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (0, 255, 255), cv2.FILLED)
 
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        # Line between thumb and first finger
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
 
-        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        # Mid circle
+        cv2.circle(img, (cx, cy), 15, (0, 255, 255), cv2.FILLED)
 
         length = math.hypot(x2 - x1, y2 - y1)
         # print(length)
 
-        if length < 50:
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+        # Hand Range 50 to 300
+        # Volume Range -65 to 0
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        volume.SetMasterVolumeLevel(vol, None)
 
+        if int(volPer) == 100:
+            cv2.circle(img, (x1, y1), 15, (0, 255, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 255, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+        if int(volPer) == 0:
+            cv2.circle(img, (cx, cy), 10, (0, 0, 255), cv2.FILLED)
+
+    print(f"Current Volume Percentage: {int(volPer)} %")
+
+    # Volume Bar and volume percentage
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    # If volume is 100, Volume Bar and volume percentage will be in green
+    if int(volPer) == 100:
+        cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+        cv2.rectangle(img, (50, int(volBar)), (85, 400),
+                      (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, f'{int(volPer)} %', (40, 450),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+
+    # If volume is 0, Volume Bar and volume percentage will be in red
+    if int(volPer) == 0:
+        cv2.rectangle(img, (50, 150), (85, 400), (0, 0, 255), 3)
+        cv2.rectangle(img, (50, int(volBar)), (85, 400),
+                      (0, 0, 255), cv2.FILLED)
+        cv2.putText(img, f'{int(volPer)} %', (40, 450),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+
+    # Calculating FPS
     cTime = time.time()
     fps = 1 / (cTime - pTime)
-    ptime = cTime
-
+    pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (40, 50),
                 cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 3)
 
